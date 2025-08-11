f563ac7bced515d1f104275462a56dfe802653b4
Felipe de Oliveira <felipeoliveiradn2@gmail.com>
2025-08-10 21:28:07 -0300

    ORder, Instante, ISO 8601 - PART 1

--- src/main/java/com/projectspringboot/projectspring/entities/User.java
+++ src/main/java/com/projectspringboot/projectspring/entities/User.java
@@ -1,16 +1,18 @@
 package com.projectspringboot.projectspring.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_user")
+@Table(name = "tb_user")	
 
 public class User implements Serializable {
 
@@ -24,11 +26,18 @@ public class User implements Serializable {
 		private String phone;
 		private String password;
 		
-		public User() {
+		// o * representa que é uma lista;
+		
+		@OneToMany(mappedBy = "client") //acessa automaticamente os pedidos feitos
+		private List<Order> orders = new ArrayList();
+		
+		// No caso de coleção, usa-se apenas o get
+		
+		public User () {
 			
 		}
 
-		public User(Long id, String name, String email, String phone, String password) {
+		public User (Long id, String name, String email, String phone, String password) {
 			this.id = id;
 			this.name = name;
 			this.email = email;
@@ -75,7 +84,10 @@ public class User implements Serializable {
 		public void setPassword(String password) {
 			this.password = password;
 		}
-
+		
+		public List<Order> getOrders() {
+			return orders;
+		}
 		@Override
 		public int hashCode() {
 			return Objects.hash(id);
@@ -91,8 +103,6 @@ public class User implements Serializable {
 				return false;
 			User other = (User) obj;
 			return Objects.equals(id, other.id);
-		}
-
-		
+		}		
 		
 }
