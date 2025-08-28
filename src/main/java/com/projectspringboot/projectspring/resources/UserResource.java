2b5dc4715961cc0a2b635580b2218a2e0e958fdb
unknown <felipeoliveiradn2@gmail.com>
2025-08-28 20:20:51 -0300

    User update

--- src/main/java/com/projectspringboot/projectspring/resources/UserResource.java
+++ src/main/java/com/projectspringboot/projectspring/resources/UserResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +36,7 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	//insere um recurso
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User obj) {
 		obj = service.insert(obj);
@@ -42,10 +44,16 @@ public class UserResource {
 		return ResponseEntity.created(uri).body(obj);
 	}
 	
+	//deleta um recurso
 	@DeleteMapping (value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+	//atualiza um recurso
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update (@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
 }
