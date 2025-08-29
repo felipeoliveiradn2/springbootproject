3b5ed7499d1336a9ac933450fec8bb870276a71c
unknown <felipeoliveiradn2@gmail.com>
2025-08-29 19:28:03 -0300

    Exception handling - delete

--- src/main/java/com/projectspringboot/projectspring/services/UserService.java
+++ src/main/java/com/projectspringboot/projectspring/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projectspringboot.projectspring.entities.User;
 import com.projectspringboot.projectspring.repositories.UserRepository;
+import com.projectspringboot.projectspring.services.exceptions.DatabaseException;
 import com.projectspringboot.projectspring.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -18,7 +21,6 @@ public class UserService {
 	
 	public List <User> findAll() {
 		return repository.findAll();
-		
 	}
 	
 	public User findById(Long id) {
@@ -31,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
@@ -44,6 +52,5 @@ public class UserService {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
 	}
 }
